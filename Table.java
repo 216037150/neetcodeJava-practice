a5de2278e1587ed90e88cb967c461593d2a45e80
216037150 <siyabongazungu95@gmail.com>
2024-12-13 09:14:45 +0200

    java multiplcation table

---
+++ Table.java
@@ -0,0 +1,11 @@
+public class Table {
+    public static void main(String[] args) {
+        int n = 12;
+        for(int i = 1; i<=n; i++){
+            System.out.println("\t");
+            for(int j = 1; j<=n; j++){
+                System.out.print(i*j + "\t" );
+            }
+        }
+    }
+}
